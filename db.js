1bd2c93349e6630e8efe39cecec3423425758215
angkiki <gabrielangzw@gmail.com>
2018-07-09 13:59:37 +0800

    updated db.js

--- db.js
+++ db.js
@@ -1,11 +1,32 @@
 const pg = require('pg');
+const url = require('url');
 
-const configs = {
-  user: 'angkiki',
-  host: '127.0.0.1',
-  database: 'project_2',
-  port: 5432
-};
+if( process.env.DATABASE_URL ){
+
+  //we need to take apart the url so we can set the appropriate configs
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  //make the configs object
+  var configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+} else {
+    const configs = {
+      user: 'angkiki',
+      host: '127.0.0.1',
+      database: 'project_2',
+      port: 5432
+    };
+}
+
 
 const poolObj = new pg.Pool(configs);
 
